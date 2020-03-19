6dfbd3851a45d93c380ba3623b0cb161faf31f28
RobertJasttrzebski <robertjastrz@gmail.com>
2020-03-19 16:46:19 +0100

    python files

---
+++ Python/Nauka1.py
@@ -0,0 +1,55 @@
+# a = 5.34
+#
+# mystring = 'abcdefghijklmnoprstuwz'
+# print(mystring[::-1])
+#
+# print('To jest imie {}'.format("Roberta" + ' i Seby'))
+
+# r = 12.232141
+# print("the result was { r:1.3f } ".format(r))
+#
+# name = 'robert'
+# # new_list =['robert', 1234, 'karolina']
+# #
+# # print(f'MOje imie to {name} ')
+# #
+# # new_list.append('Hubi')
+# #
+# # print(new_list[1::])
+# # print(new_list)
+# # new_list.pop(0)
+# # print(new_list)
+# #
+# # # True and False
+# # b = True
+# # a = False
+# # print(b)
+
+# **************** IF ELIF ELSE************************
+# place = 'w pracy'
+# if place == 'dom':
+#     print("jestem w domu")
+# elif place == 'bank':
+#     print('jestem w banku')
+# elif place == 'park':
+#     print ('jestem w parku')
+# else:
+#     print("jestem w innym miejscu")
+#
+
+
+# ***************FOR LOOP ***************************
+# my_list= [1,2,3,4,5,6,7,8,9,10]
+# for lista in my_list:
+#     print(lista)
+
+
+# lista z tuple**********
+# moja_lista = [(1,2,3,4),(2,3,5,6),(5,2,4,5)]
+# for a,b,c,d in moja_lista:
+#     print(d)
+#
+# *****dictionary value unpacking***************
+d= {'k1':2,'k2': 6,'k3': 9}
+for b in d.items():
+    print(b)
