f124ced227138ade9d99c81a4141d41bfd34e7c5
RobertJasttrzebski <robertjastrz@gmail.com>
2020-08-12 19:40:42 +0200

    calculator event listener

--- JavaScript/JavasScript basic2 -Maximilian Schwarzmüller/basics-01-starting-project/assets/scripts/app.js
+++ JavaScript/JavasScript basic2 -Maximilian Schwarzmüller/basics-01-starting-project/assets/scripts/app.js
@@ -1,4 +1,38 @@
 const domyslnaWartosc = 0;
-let wynik = 3;
-let opisZmiennej = ` wynik = ${domyslnaWartosc}`;
-outputResult(wynik, opisZmiennej);
+let wynik = domyslnaWartosc;
+
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+function add() {
+  const enteredNum = getUserNumberInput();
+  const opis = `${wynik} + ${enteredNum}`;
+  wynik = wynik + enteredNum;
+  outputResult(wynik, opis);
+}
+
+function subtract() {
+  const enteredNum = getUserNumberInput();
+  const opis = `${wynik} - ${enteredNum}`;
+  wynik = wynik - enteredNum;
+  outputResult(wynik, opis);
+}
+
+function multiply() {
+  const enteredNum = getUserNumberInput();
+  const opis = `${wynik} * ${enteredNum}`;
+  wynik = wynik * enteredNum;
+  outputResult(wynik, opis);
+}
+
+function divide() {
+  const enteredNum = getUserNumberInput();
+  const opis = `${wynik} / ${enteredNum}`;
+  wynik = wynik / enteredNum;
+  outputResult(wynik, opis);
+}
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
