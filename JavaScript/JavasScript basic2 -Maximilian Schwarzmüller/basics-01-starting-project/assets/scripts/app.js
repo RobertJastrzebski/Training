36cf543e0130b3fe33112c30ea626baa55aadbb8
RobertJasttrzebski <robertjastrz@gmail.com>
2020-08-17 20:04:58 +0200

    log function

--- JavaScript/JavasScript basic2 -Maximilian Schwarzmüller/basics-01-starting-project/assets/scripts/app.js
+++ JavaScript/JavasScript basic2 -Maximilian Schwarzmüller/basics-01-starting-project/assets/scripts/app.js
@@ -1,36 +1,58 @@
 const domyslnaWartosc = 0;
 let wynik = domyslnaWartosc;
+let logEntries = [];
 
 function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
+function writeToLog(operator, wynikPrzed, liczba, wynikPo) {
+  const logEntry = {
+    operation: operator,
+    prevResult: wynikPrzed,
+    number: liczba,
+    result: wynikPo,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
 function add() {
   const enteredNum = getUserNumberInput();
   const opis = `${wynik} + ${enteredNum}`;
+  wynikPrzed = wynik;
   wynik += enteredNum;
   outputResult(wynik, opis);
+  writeToLog('ADD',wynikPrzed,enteredNum,wynik);
+ 
+  
 }
 
 function subtract() {
   const enteredNum = getUserNumberInput();
   const opis = `${wynik} - ${enteredNum}`;
+  wynikPrzed = wynik;
   wynik -= enteredNum;
   outputResult(wynik, opis);
+  writeToLog('substract',wynikPrzed,enteredNum,wynik);
 }
 
 function multiply() {
   const enteredNum = getUserNumberInput();
   const opis = `${wynik} * ${enteredNum}`;
+  wynikPrzed = wynik;
   wynik *= enteredNum;
   outputResult(wynik, opis);
+  writeToLog('multiply',wynikPrzed,enteredNum,wynik);
 }
 
 function divide() {
   const enteredNum = getUserNumberInput();
   const opis = `${wynik} / ${enteredNum}`;
+  wynikPrzed = wynik;
   wynik /= enteredNum;
   outputResult(wynik, opis);
+  writeToLog('divide',wynikPrzed,enteredNum,wynik);
 }
 addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", subtract);
