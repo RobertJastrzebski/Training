d3af3ccd1fcf08875a6dea396f39febda0302db3
RobertJasttrzebski <robertjastrz@gmail.com>
2020-11-02 14:26:31 +0100

    classes arrays

--- C#/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/Program.cs
@@ -1,36 +1,55 @@
-﻿using System;
+﻿using ConsoleApp1.Math;
 using System.Threading;
 
 namespace ConsoleApp1
 {
-    
-
-    public class Person
-    {
-        public string FirstName;
-        public string LastName;
-
-
-        public void introduce()
-        {
-            Console.WriteLine("My name is "+ FirstName + " " + LastName);
-        }
-
-
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
-            john.introduce();
+            //classes------------------------------
+
+            //Person john = new Person();
+            //john.FirstName = "John";
+            //john.LastName = "Smith";
+            //john.introduce();
+
+            //var cal = new calculator();
+            //var result =  cal.Add(2, 3);
+            //System.Console.WriteLine(result);
+
+
+            //Arrays------------------------------
+
+           //var numbers = new int[3];
+           // numbers[0] = 1;
+           // numbers[1] = 22;
+           // numbers[2] = 33;
+
+           // System.Console.WriteLine(numbers[0]);
+           // System.Console.WriteLine(numbers[1]);
+           // System.Console.WriteLine(numbers[2]);
+
+
+           // var flags = new bool[3];
+           // flags[0] = true;
+           // System.Console.WriteLine(flags[0]);
+           // System.Console.WriteLine(flags[1]);
+           // System.Console.WriteLine(flags[2]);
+
+           // var names = new string[3] { "jack", "john", "Mary" };
+           // System.Console.WriteLine(names[0]);
+           // System.Console.WriteLine(names[1]);
+           // System.Console.WriteLine(names[2]);
+
+            //Strings-----------------------------------------
+
+
+
         }
+
+
     }
 
 }
-
-
-
