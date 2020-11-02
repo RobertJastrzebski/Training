22a6d04de9a0a56c7276a7861ee3ba753b7a002b
RobertJasttrzebski <robertjastrz@gmail.com>
2020-11-02 13:14:34 +0100

    classes

---
+++ C#/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    
+
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+
+        public void introduce()
+        {
+            Console.WriteLine("My name is "+ FirstName + " " + LastName);
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.introduce();
+        }
+    }
+
+}
+
+
+
