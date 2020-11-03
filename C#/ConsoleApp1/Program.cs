409bc9f00e72858461db9c8fee97883f8810a6a7
RobertJasttrzebski <robertjastrz@gmail.com>
2020-11-03 10:44:26 +0100

    Reference type

--- C#/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/Program.cs
@@ -2,7 +2,16 @@
 using System.Threading;
 
 namespace ConsoleApp1
-{
+ {
+    //Enum----------------------
+    //public enum Shippinmethod
+    //{
+    //    regular=1,
+    //    registered=2,
+    //    express =3
+        
+    //}
+
 
     class Program
     {
@@ -45,24 +54,43 @@ namespace ConsoleApp1
 
             //Strings-----------------------------------------
 
-             
-
-        //    var firstName = "Mosh";
-        //    var lastName = "nowak";
-        //    var fullName = firstName + " " + lastName;
-        //    System.Console.WriteLine(fullName);
-
-        //    string str = "rob";
-        //    string strFormat = string.Format("my nam is {0}  {1}", str, 2);
-        //    System.Console.WriteLine(strFormat);
-
-        //    var names = new string[3] { "john", "jack", "Mary" };
-        //    var formattedNames = string.Join(",", names);
-        //    System.Console.WriteLine(formattedNames);
 
 
-            
-        //}
+            //    var firstName = "Mosh";
+            //    var lastName = "nowak";
+            //    var fullName = firstName + " " + lastName;
+            //    System.Console.WriteLine(fullName);
+
+            //    string str = "rob";
+            //    string strFormat = string.Format("my nam is {0}  {1}", str, 2);
+            //    System.Console.WriteLine(strFormat);
+
+            //var names = new string[3] { "john", "jack", "Mary" };
+            //var formattedNames = string.Join(",", names);
+            //    System.Console.WriteLine(formattedNames);
+
+
+
+
+            ////Enum--------------------------------------------
+            ///
+            //var method = Shippinmethod.express;
+            //System.Console.WriteLine((int)method);
+
+            //var metodId = 3;
+            //System.Console.WriteLine((Shippinmethod) metodId );
+
+            //System.Console.WriteLine(method);
+
+            //reference type
+            //var array1 = new int[3] { 1, 2, 3 };
+            //var array2 = array1;
+            //array2[0] = 0;
+            //System.Console.WriteLine(string.Format("array1[0]:{0},array2[0]:{1}",array1[0],array2[0]));
+
+
+
+        }
 
 
     }
