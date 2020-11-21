08da46550a06152094fbe3240b32a470615e97f4
RobertJasttrzebski <robertjastrz@gmail.com>
2020-11-21 18:13:59 +0100

    Unit test v2.

---
+++ C#/UnitTestProject1/PrzekazywanieParametrow.cs
@@ -0,0 +1,18 @@
+﻿using KartaOcenFilmow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class PrzekazywanieParametrow
+    {
+        [TestMethod]
+        public void przekazywanieParWartKlasa()
+
+        {
+            //karta karta1 = new karta();
+
+        }
+    }
+}
