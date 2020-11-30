ae8c6b97122af4432144f15e98592de5208c3f23
RobertJasttrzebski <robertjastrz@gmail.com>
2020-11-30 20:15:05 +0100

    kalkulator

---
+++ C#/2. Small Aplications/1.Kalkulator/Kalkulator/CalcEngine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kalkulator
+{
+    public class CalcEngine
+    {
+        public double calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double result;
+            switch (argOperation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+                case "subtract":
+                case "-":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+                case "division":
+                case "/":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+                case "multipy":
+                case "*":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                default:
+                    throw new InvalidOperationException("Specified operation is not recognized");
+            }
+            return result;
+        }
+
+
+    }
+}
---
+++ C#/2. Small Aplications/1.Kalkulator/Kalkulator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalcEngine calcEngine = new CalcEngine();
+
+                Console.Write("Provide one number:");
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.Write("Provide second number:");
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.Write("Provide operation symbol:");
+                string operation = Console.ReadLine();
+
+                double result = calcEngine.calculate(operation, firstNumber, secondNumber);
+                Console.WriteLine("The final result is : {0} {1} {2} = {3}",firstNumber,operation,secondNumber,result );
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+            
+        }
+    } 
+}
