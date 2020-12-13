a207ab98930cf4cdf2972994f67fbf6d0fa5c8f0
RobertJasttrzebski <robertjastrz@gmail.com>
2020-12-13 12:15:55 +0100

    loops 13.12.2020

---
+++ C#/2. Small Aplications/2.Word Unscrambler/WordUnscrambler/LoopsExercise.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+
+namespace WordUnscrambler
+{
+
+    class LoopsExercise
+    {
+        public int Loops()
+        {
+            int[] array = {1, 5, 6, 7, 8, 98, 9};
+
+
+            List<int> lista = new List<int>() {2, 2, 4, 5, 6, 45, 47};
+
+
+
+            //---------------------------------------------------------------------------------
+            Console.WriteLine("foreach loop");
+            foreach (var liczba in array)
+            {
+                Console.WriteLine(liczba);
+            }
+
+            //----------------------------------------------------------------------------------
+            Console.WriteLine("for loop");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+
+
+            }
+
+            //---------------------------------------------------------------------------------
+            Console.WriteLine("while loop");
+
+            int Index = 0;
+            while (Index < array.Length)
+            {
+
+                Console.WriteLine(array[Index]);
+                Index++;
+
+            }
+
+            //---------------------------------------------------------------------------------
+
+            string[] lines = {"this is the first line", "This is the second", "This is the third line"};
+            File.WriteAllLines("MyFirstFile.txt", lines);
+
+
+
+            string[] fileContent = File.ReadAllLines("MyFirstFile.txt");
+
+            foreach (var zdanie in fileContent)
+            {
+                Console.WriteLine(zdanie);
+            } 
+
+
+            return 0;
+        }
+
+
+    }
+}
+
+        
+
+        
+
+
+
+
+
+       
+
+
+
+    
+
+
--- C#/2. Small Aplications/2.Word Unscrambler/WordUnscrambler/Program.cs
+++ C#/2. Small Aplications/2.Word Unscrambler/WordUnscrambler/Program.cs
@@ -12,42 +12,9 @@ namespace WordUnscrambler
         static void Main(string[] args)
         {
 
-            int[] array = {1, 5, 6, 7, 8, 98, 9};
+         //var nowa = new LoopsExercise();
+         //nowa.Loops();
 
-
-            List<int> lista = new List<int>();
-
-            Console.WriteLine("foreach loop");
-            foreach (var liczba in array)
-            {
-                Console.WriteLine(liczba);
-            }
-
-            Console.WriteLine("for loop");
-            for (int i = 0; i < array.Length; i++) 
-            {
-                Console.WriteLine(array[i]);
-                
-            }
-
-            Console.WriteLine("while loop");
-            int Index = 0;
-            while (Index<array.Length)
-            {
-
-                Console.WriteLine(array[Index]);
-                Index++;
-                
-            }
-
-            string[] lines = {"this is the first line", "This is the second", "This is the third line"};
-            File.WriteAllLines("MyFirstFile.txt",lines);
-            string[] fileContent =File.ReadAllLines("MyFirstFile.txt");
-
-            foreach (var zdanie in File.ReadAllLines("MyFirstFile.txt"))
-            {
-                Console.WriteLine(zdanie);
-            }
 
 
         }
