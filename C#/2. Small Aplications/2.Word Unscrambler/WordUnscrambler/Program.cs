7d1b4346c8a0daa05494228215ee6df07f37bca7
RobertJasttrzebski <robertjastrz@gmail.com>
2020-12-22 17:11:02 +0100

    Angular

--- C#/2. Small Aplications/2.Word Unscrambler/WordUnscrambler/Program.cs
+++ C#/2. Small Aplications/2.Word Unscrambler/WordUnscrambler/Program.cs
@@ -17,6 +17,54 @@ namespace WordUnscrambler
 
 
 
+         bool kontynuowacRozszyfrowanie = true;
+         do
+         {
+             Console.WriteLine("Prosze wpisz opcje F dla pliku oraz M dla wprowadzenia manualnego");
+             var odpowiedzUzytkownika = Console.ReadLine() ?? string.Empty;
+
+             switch (odpowiedzUzytkownika.ToUpper())
+             {
+                    case "F":
+                        Console.Write("Wpisz nazwe pliku zaszyfrowanych słów.");
+                        WykonajZaszyfrowaneSlowaZpliku();
+                        break;
+                    case "M":
+                        Console.Write("Wpisz zaszyfrowane słowa ręcznie.");
+                        WykonajZaszyfrowaneSłowaWypisaneRecznie();
+                        break;
+                    default:
+                        Console.Write("Nie rozpoznano takiej opcji.");
+                        break;
+             }
+                var kontynuacjaZapytania = string.Empty;
+                do
+                {
+                    Console.WriteLine(" Czy chcesz kontynuować ? Y/N");
+                    kontynuacjaZapytania = (Console.ReadLine() ?? string.Empty);
+
+                } while (!kontynuacjaZapytania.Equals("Y",StringComparison.OrdinalIgnoreCase)&& 
+                         !kontynuacjaZapytania.Equals("N",StringComparison.OrdinalIgnoreCase));
+
+                kontynuowacRozszyfrowanie = kontynuacjaZapytania.Equals("Y",StringComparison.OrdinalIgnoreCase);
+
+             
+
+
+         } while (kontynuowacRozszyfrowanie);
+
+
+
+        }
+
+        private static void WykonajZaszyfrowaneSłowaWypisaneRecznie()
+        {
+            
+        }
+
+        private static void WykonajZaszyfrowaneSlowaZpliku()
+        {
+            
         }
     }
 }
