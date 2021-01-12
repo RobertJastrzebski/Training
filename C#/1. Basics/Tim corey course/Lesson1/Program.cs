d1c3028a908bb299330388aa8a806f33347d0d93
RobertJasttrzebski <robertjastrz@gmail.com>
2021-01-12 17:03:39 +0100

    Tim Corey Course 12.01.2020

---
+++ C#/1. Basics/Tim corey course/Lesson1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your first name?");
+            //string firstName = Console.ReadLine();
+            //Console.WriteLine("What is you Second name?");
+            //string secondName = Console.ReadLine();
+            //Console.WriteLine("what is your age ?");
+            //string ageText = Console.ReadLine();
+            
+            //bool isValidAge = int.TryParse(ageText,out int age);
+            //if (isValidAge)
+            //{
+            //    Console.WriteLine("your age is " + age);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This is not a numeric value");
+            //    return;
+            //}
+
+
+            //string formatedName = String.Empty;
+            //if (firstName.ToLower() == "rob" || firstName.ToLower() == "bob")
+            //{
+            //    formatedName = $"Profesor {firstName}";
+                
+            //}
+            //else
+            //{
+            //    formatedName = firstName;
+                
+            //}
+
+
+
+            //if (age>18)
+            //{
+            //    Console.WriteLine($"hello you age is {age} you can enter{formatedName}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"hello {formatedName} your age is {age} .You need to wait {18-age} years to enter");
+            //}
+
+            string response;
+            do
+            {
+                Console.WriteLine("What is you name?");
+                string name = Console.ReadLine();
+                if (name.ToLower() =="tim")
+                {
+                    Console.WriteLine("Hello Proffesor");
+                }
+                else
+                {
+                    Console.WriteLine($"hi {name}");
+                }
+
+                Console.WriteLine("If you want to quit type exit");
+                 response = Console.ReadLine();
+
+            } while (response.ToLower()!="exit");
+
+            
+
+
+
+        }
+    }
+}
---
+++ C#/1. Basics/Tim corey course/Lesson2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstNames = new string[5];
+
+            firstNames[0] = "Rob";
+
+            Console.ReadLine();
+
+        }
+    }
+}
