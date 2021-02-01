d14c729074a8da4c31b446870b2ef5587eb1f332
RobertJasttrzebski <robertjastrz@gmail.com>
2021-02-01 19:43:18 +0100

    Gra w Statki dokumentacja oraz class library.

---
+++ C#/2. Small Aplications/GraWStatki/GraWStatkiLibrary/Modele/GraczInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraWStatkiLibrary.Modele
+{
+    public class GraczInfo
+    {
+        public string ImieGracza { get; set; }
+        public List<MiejsceNaSiatce> LokalizacjaStatków { get; set; }
+
+        public List<MiejsceNaSiatce> LokalizacjaStrzałów { get; set; }
+    }
+}
---
+++ C#/2. Small Aplications/GraWStatki/GraWStatkiLibrary/Modele/MiejsceNaSiatce.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraWStatkiLibrary.Modele
+{
+    public class MiejsceNaSiatce
+    {
+        public string MiejsceLitera { get; set; }
+        public int MiejsceCyfra { get; set; }
+
+
+        public StatusPola Status { get; set; } = StatusPola.puste; 
+
+    }
+}
